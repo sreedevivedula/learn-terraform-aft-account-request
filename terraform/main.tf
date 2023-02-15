fde3bb91dc392abd57cdf5cc75f8d254108e9966
Sreedevi <surya_sreedevi_vedula@apple.com>
2023-02-15 16:04:48 +0530

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
+    AccountEmail              = "sreedevi+aft+one@thoughtworks.com"
+    AccountName               = "sandbox-aft-one"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "sreedevi+aft+one@thoughtworks.com"
     SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserLastName           = "AFT One"
   }
 
   account_tags = {
